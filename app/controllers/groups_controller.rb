c00c723627eb0ebb272082044cbefaf8b0bb4ee0
Precious chiedu <udegbue69@gmail.com>
2020-07-21 10:24:29 +0100

    added sum totals

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,7 @@
 class GroupsController < ApplicationController
   before_action :set_deal, only: %i[show edit update destroy]
   before_action :authenticate_user!
-  helper_method :filter_groups
+  helper_method :sum_of_deals
 
   # GET /deals
   # GET /deals.json
@@ -14,7 +14,7 @@ class GroupsController < ApplicationController
   # GET /deals/1
   def show
     @group = Group.find(params[:id])
-    @group_deals = @group.deals
+    @group_deals = @group.deals.includes(groups: :icon_attachment)
   end
 
   # GET /deals/new
@@ -77,8 +77,12 @@ class GroupsController < ApplicationController
     params.require(:group).permit(:name, :icon)
   end
 
-  def filter_groups
-    @group = Group.find(params[:id])
-    puts @group.name
+  def sum_of_deals
+    @user_deals_value = 0
+    @group_deals = @group.deals.includes(groups: :icon_attachment)
+    @group_deals.each do |a|
+      @user_deals_value += a.amount
+    end
+    @user_deals_value
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  helper_method :sum_deals
+  helper_method :sum_external_deals
+
   def index
     @users = User.all
   end
@@ -13,22 +16,44 @@ class UsersController < ApplicationController
 
   def make_deals
     @user = current_user
-    @sum = @user.deals.sum(:amount)
     @deals = Deal.all
-    @user_deals = Deal.joins(:dealings).where(author_id: current_user.id).includes(:dealings, groups: :icon_attachment).to_a
+    @user_deals = Deal.joins(:dealings).where(author_id: current_user.id).includes(groups: :icon_attachment).to_a
+    @sum = @user.deals.sum(:amount)
+    sum_deals
   end
 
+  
   def external
     @user = current_user
     @sum = @user.deals.sum(:amount)
     @deals = Deal.all
     @user_deals = Deal.joins(:dealings).where(author_id: current_user.id)
     @external = current_user.deals - @user_deals
+    sum_external_deals
   end
 
   private
 
   def deal_params
     params.require(:deal).permit(:name, :amount, { group_ids: [] })
+  end
+
+  def sum_deals
+    @user_deals_value = 0
+    @user_deals = Deal.joins(:dealings).where(author_id: current_user.id).includes(:dealings, groups: :icon_attachment).to_a
+    @user_deals.each do |a|
+      @user_deals_value += a.amount
+    end
+    @user_deals_value
+  end
+
+  def sum_external_deals
+    @user_deals_value = 0
+    @user_deals = Deal.joins(:dealings).where(author_id: current_user.id).includes(:dealings, groups: :icon_attachment).to_a
+    @external = current_user.deals - @user_deals
+    @external.each do |a|
+      @user_deals_value += a.amount
+    end
+    @user_deals_value
   end
 end
