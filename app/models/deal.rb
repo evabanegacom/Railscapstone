13bae2b5f65cd995adf4f81d37bd2979fe8b34d1
Precious chiedu <udegbue69@gmail.com>
2020-07-22 05:05:22 +0100

    fixed validation errors

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -2,9 +2,15 @@ class Deal < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :dealings, dependent: :destroy
   has_many :groups, through: :dealings
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
   validates :amount, presence: true
   has_many :comments, dependent: :destroy
+
+  validate :uniqueness_of_name_and_author
+
+  def uniqueness_of_name_and_author
+    errors.add(:name, 'must be unique') if author.deals.where(name: name).exists?
+  end
 
   default_scope { order('created_at DESC') }
 end
